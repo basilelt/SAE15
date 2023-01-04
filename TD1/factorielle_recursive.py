69fd93deb27010f1b7486481f35237f67916d9d2
basilelt <lethiecb@protonmail.com>
2023-01-04 15:42:44 +0100

    td1

--- TD1/factorielle_recursive.py
+++ TD1/factorielle_recursive.py
@@ -1,7 +1,8 @@
 n = int(input("Entrez un entier : "))
 fact = 1
 
-for i in range(n):
-    fact = fact * (i + 1)
+def facto(n, fact):
+    if i > n:
+        fact = fact * (i + 1)
 
 print(f"La factorielle de {n} est {fact}.")
